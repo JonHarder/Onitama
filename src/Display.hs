ed3c0701498ad8da880387370d7955de06d17981
Jonathan Harder <jonharder6@gmail.com>
2020-07-23 23:21:29 -0500

    fix backwards board, move label

--- src/Display.hs
+++ src/Display.hs
@@ -20,17 +20,17 @@ viewSpace (Ocupied (team, piece)) = pictures [viewSpace Empty, color (teamToColo
 
 
 viewRow :: Float -> [Space] -> Picture
-viewRow yOffset = pictures . zipWith drawSpace [-100, -50 ..]
+viewRow yOffset = pictures . zipWith drawSpace [100, 50 ..]
   where drawSpace xOffset = translate xOffset yOffset . viewSpace
 
 
 viewBoard :: [[Space]] -> Picture
-viewBoard = pictures . zipWith viewRow [-100, -50 ..]
+viewBoard = pictures . zipWith viewRow [100, 50 ..]
 
 
 view :: Game -> Picture
 view game =
   pictures
     [ viewBoard $ spaces game
-    , scale 0.25 0.25 $ translate (-300) (-200) $ text $ show $ selected game
+    , scale 0.25 0.25 $ translate (-500) 520 $ text $ show $ selected game
     ]
